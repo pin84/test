f028b47c33faddd86405c35252d728ac0a93c829
pin84 <qp1984@live.com>
2020-04-18 16:30:00 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="img-box" v-for="(item,index) in 6" :key="index">
+      <img :src="require(`../assets/images/${index+1}.jpg`)" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data(){
+    return {
+      imgUrl:'../assets/images/'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.home
+  width 100%
+  .img-box
+    width 100%
+    img
+      width 100%
+
+
+</style>
