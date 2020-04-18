0a9cb85d4401cdf6ad895c21b61c1aab31553253
pin84 <qp1984@live.com>
2020-04-18 16:32:14 +0800

    save

---
+++ dist/precache-manifest.af3ab9bd8b705501a6bdb8cd56f89124.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acab501684e455e1912e",
+    "url": "/css/app.944b9cfc.css"
+  },
+  {
+    "revision": "da77392f5e03a9d42316b6beee830ee2",
+    "url": "/img/1.da77392f.jpg"
+  },
+  {
+    "revision": "d8cb6f9ac2b9dbc6c3185aeb40fb16bb",
+    "url": "/img/2.d8cb6f9a.jpg"
+  },
+  {
+    "revision": "09a3d3a012078e3d29c82fbba0e81445",
+    "url": "/img/3.09a3d3a0.jpg"
+  },
+  {
+    "revision": "d78eb4b2044539d6d2e49c1696ad6856",
+    "url": "/img/4.d78eb4b2.jpg"
+  },
+  {
+    "revision": "419f980251f809247d4b0a776db701b2",
+    "url": "/img/5.419f9802.jpg"
+  },
+  {
+    "revision": "28308f4d742db4923b9171291835e4af",
+    "url": "/img/6.28308f4d.jpg"
+  },
+  {
+    "revision": "0e919cc3c9870310a6419b0637c90180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acab501684e455e1912e",
+    "url": "/js/app.8ddadba9.js"
+  },
+  {
+    "revision": "e21803f210b47e79d03f",
+    "url": "/js/chunk-2d22d746.c9dcbfb3.js"
+  },
+  {
+    "revision": "4443e9b5a1ff87a2c874",
+    "url": "/js/chunk-vendors.735d88dc.js"
+  },
+  {
+    "revision": "9bcd572eeadb7a5478e7af9b7b47d5c7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
